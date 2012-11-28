4daddf1b15b2563c5cc31c21dc2b3e8c6ba2227a
Ryan Yogan <ryanyogan@gmail.com>
2012-11-27 20:13:06 -0600

    Changed to heroku env

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,3 @@
-  #uri = URI.parse(ENV["REDISTOGO_URL"])
-  uri = "redis://localhost:6379"
+  uri = URI.parse(ENV["REDISTOGO_URL"])
   REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
   Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
